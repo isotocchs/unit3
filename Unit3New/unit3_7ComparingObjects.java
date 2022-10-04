dcc19aa0b3585a7117b5e1b6572744c6439d0c38
isotocchs <89474695+isotocchs@users.noreply.github.com>
2022-10-04 08:43:13 -0400

    save

---
+++ Unit3New/unit3_7ComparingObjects.java
@@ -0,0 +1,19 @@
+
+public class unit3_7ComparingObjects {
+	
+	//Object info and memory
+	
+	String name;
+	int age;
+	int height;
+	//Constructor
+	public unit3_7ComparingObjects(String nm, int ag, int ht) {
+		
+		name = nm;
+		age = ag;
+		height = ht;
+		
+	}
+	
+
+}
